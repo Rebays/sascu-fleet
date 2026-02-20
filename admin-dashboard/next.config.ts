8dca21025d718c873f899bdfdd68efdf0321c9cb
Joshua Zobule <joshwhizkid@gmail.com>
2026-02-20 14:11:49 +1100

    admin dashboard redirect to client --- REVERT LATER

--- admin-dashboard/next.config.ts
+++ admin-dashboard/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://sascu-book.vercel.app/',
+        permanent: false,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
